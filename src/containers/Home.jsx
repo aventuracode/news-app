ec8037ebf1a94fcbd3e8df1849bc120596b80efa
aventura-gruposabra <aventura@gruposabra.com>
2022-07-04 14:15:58 -0300

    [add] datafake

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -4,10 +4,12 @@ import WrapTitle from '../components/WrapTitle/WrapTitle';
 import AppContext from '../context/AppContext';
 import ButtonUILg from '../components/UIComponents/ButtonUILg';
 import { URL_TOPHEADLINES } from '../components/helpers/requester';
+import { data } from '../db';
 
 const Home = () => {
 	const { state, addToNews } = useContext(AppContext);
 	const { country, news } = state;
+	const datafake = JSON.parse(data);
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
@@ -24,7 +26,7 @@ const Home = () => {
 	return (
 		<>
 			<WrapTitle section='home' />
-			<ListOfArticle news={news} />
+			<ListOfArticle news={datafake} />
 			<div className='wrapButton'>
 				<ButtonUILg />
 			</div>
--- src/containers/Search.jsx
+++ src/containers/Search.jsx
@@ -4,16 +4,18 @@ import WrapTitle from '../components/WrapTitle/WrapTitle';
 import ListOfArticle from '../components/ListOfArticles/ListOfArticles';
 import Searcher from '../components/Searcher/Searcher';
 import ButtonUILg from '../components/UIComponents/ButtonUILg';
+import { data } from '../db';
 import './Container.css';
 
 const Search = () => {
 	const { state } = useContext(AppContext);
 	const { news } = state;
+	const datafake = JSON.parse(data);
 	return (
 		<>
 			<Searcher />
 			<WrapTitle section='search' />
-			<ListOfArticle news={news} />
+			<ListOfArticle news={datafake} />
 			<div className='wrapButton'>
 				<ButtonUILg />
 			</div>
